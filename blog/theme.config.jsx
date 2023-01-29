4f7f27125fa7e0be2317aec735410e0e248ae85f
Mauricio Robayo <rfmajo@gmail.com>
2023-01-28 20:44:31 -0500

    Update footer

--- blog/theme.config.jsx
+++ blog/theme.config.jsx
@@ -16,26 +16,8 @@ export default {
   },
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <abbr
-        title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-        style={{ cursor: 'help' }}
-      >
-        CC BY-NC 4.0
-      </abbr>{' '}
-      {new Date().getFullYear()} © Mauricio Robayo.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
+      All I wanna do is go the distance. See that bell ring and I'm still
+      standing.
     </small>
   ),
   title: 'Mauricio Robayo',
